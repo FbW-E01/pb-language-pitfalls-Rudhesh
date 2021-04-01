afd3e63b29b85a3366b90d33d715a2732827c175
dci <rudeshdhote@gmail.com>
2021-04-01 13:17:49 +0200

    initial commit

---
+++ start.js
@@ -0,0 +1,19 @@
+// What does NaN stand for Not a Number
+
+console.log(isNaN("hello"));
+
+console.log(isNaN(3));
+
+
+// What is the type of NaN?
+// it is a numeric data type whose value cannot be represented using actual numbers.
+
+const result = (0.1 * 0.2)  
+
+console.log(result.toFixed(3));
+
+console.log(Infinity / 0);
+
+console.log(Infinity / Infinity);
+
+console.log(1 / 0); 
